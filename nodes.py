f4c2006dbd747a20b435ae0a92be68f8f506d83b
joelschum@gmail.com <joelschum@gmail.com>
2015-01-21 19:41:37 +0100

    Fixed frequency modulation

--- nodes.py
+++ nodes.py
@@ -4,9 +4,9 @@ from synth import SynthParameters
 import numpy as np
 
 @registerFunction
-def sin(params:SynthParameters=None, frequencyShift:np.ndarray=0.0, frequency:float=440, amplitude:float=1.0):
+def sin(params:SynthParameters=None, modulation:np.ndarray=0.0, frequency:float=440, amplitude:float=1.0):
 	t = np.linspace(0.0, params.length, params.samples)
-	return amplitude*np.sin(2 * np.pi * (frequency+frequencyShift)*t)
+	return amplitude*np.sin(2 * np.pi * frequency * t + modulation)
 	
 @registerFunction
 def rectangle(params:SynthParameters=None, frequency:float=440, amplitude:float=1.0, duty:float=0.5):
@@ -19,10 +19,19 @@ def clamp(channel:np.ndarray=0.0, level:float=1.0):
 	return np.clip(channel, -level, level)
 	
 @registerFunction
-def mix(channelA:np.ndarray=0.0, channelB:np.ndarray=0.0, mixA:float=0.5, mixB:float=0.5):
+def mix2(channelA:np.ndarray=0.0, channelB:np.ndarray=0.0, mixA:float=0.5, mixB:float=0.5):
 	return (channelA*mixA + channelB*mixB)
 	
-#sawtooth, triangle, whistle, whiteNoise, rectangle (frequency, duty cycle)
+@registerFunction
+def mix(channelA:np.ndarray=0.0, channelB:np.ndarray=0.0, mix:float=0.5):
+	return (channelA*(1-mix) + channelB*mix)
+	
+@registerFunction
+def sawtooth(params:SynthParameters=None, frequency:float=440, amplitude:float=1.0):
+	t = np.linspace(0.0, params.length, params.samples)*frequency
+	return ((t - np.floor(t)) * 2.0 - 1.0) * amplitude
+	
+	
+#triangle, whistle, whiteNoise
 #delay, cheapReverb
-#mix
-#step -> rectangle, linear, exponential, 
+#step -> rectangle, linear, exponential
