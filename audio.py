df9efe0089d8a57ca3046f854f68f092b54fec5e
Jonas Lieb <lieb.jonas@gmail.com>
2015-01-23 12:19:27 +0100

    Apparently fixed crashing audio by replacing float32 with int16

--- audio.py
+++ audio.py
@@ -1,27 +1,47 @@
 import pyaudio
 import numpy as np
+import sys
+import time
 
 _pA = None
+_streams = []
 		
 def initAudio():
 	global _pA
 	_pA = pyaudio.PyAudio()
 	
-def getCallback(buffer):
+def getCallback(sound):
+	buffer = (sound*32767).astype(np.int16).tobytes()[:]
 	cursor = 0
 	def streamCallback(input, frameCount, timeInfo, statusFlags):
 		nonlocal cursor
 		nonlocal buffer
 	
-		frameCount *= 4 # convert to bytes
+		bytesCount = 2*frameCount 
 		playBuf = buffer[cursor:]
-		cursor += frameCount
-		if len(playBuf) < frameCount:
-			return (playBuf + b"\x00"*(frameCount - len(playBuf)), pyaudio.paComplete)
+		cursor += bytesCount
+		
+		if len(playBuf) < bytesCount:
+			data = playBuf + b"\x00"*(bytesCount - len(playBuf))
+			task = pyaudio.paComplete
 		else:
-			return (playBuf[:frameCount], pyaudio.paContinue)
+			data = playBuf[:bytesCount]
+			task = pyaudio.paContinue
+
+		assert(len(data) == bytesCount)
+		return (data, task)
+		
 	return streamCallback
 	
 def play(buffer, sampleRate):
-	stream = _pA.open(channels=1, rate=sampleRate, output=True, format=pyaudio.paFloat32, stream_callback=getCallback(buffer.astype(np.float32).tobytes()[:]))
+	stream = _pA.open(channels=1, rate=sampleRate, output=True, format=pyaudio.paInt16, stream_callback=getCallback(buffer))
 	stream.start_stream()
+	
+	_streams.append(stream)
+	
+	for stream in _streams:
+		if not stream.is_active():
+			stream.stop_stream()
+			stream.close()
+			_streams.remove(stream)
+			del stream
